ee2772ed6427f2f5502b382d6e1537e96d9abfb6
Manuel Cerqueira da Silva <67638640+ManelMCCS@users.noreply.github.com>
2021-02-24 23:52:52 +0000

    Update main.c

--- main.c
+++ main.c
@@ -65,12 +65,15 @@ char* getPackageManager()
 	
 	printf("\nID_LIKE:|%s| \n\n", ID_LIKE);
 	
+	char* manager;
+	manager = calloc();
+	
 	if (strcmp("debian\0", ID_LIKE) == 0 ) if(DEBUG == 1) printf("apt \n");
 	else if (strcmp("arch\0", ID_LIKE) == 0 ) if(DEBUG == 1) printf("pacman -S");
 
 
 
-	return NULL;
+	return manager;
 }
 
 
