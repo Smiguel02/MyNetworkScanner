4327622a0571b6412d8b72257458b80890a04929
ManelMCCS <manuelmagalhaes7@gmail.com>
2021-02-27 19:59:39 +0000

    organizing function in files

---
+++ info.h
@@ -0,0 +1,16 @@
+#if !defined(INFO_F)
+#define INFO_F
+
+#include <stdio.h>
+#include <stdlib.h> 
+#include <string.h>
+
+char* StrHandler(char *ini, int* maxlen, char end);
+
+char* getPackageManager();
+
+char* getIP();
+
+#endif // INFO_F
+
+
