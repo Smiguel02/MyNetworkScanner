406538ab17ba6e7891febf81d097309afeaf51a9
ManelMCCS <manuelmagalhaes7@gmail.com>
2021-02-28 11:18:15 +0000

    working on python...

--- read.py
+++ read.py
@@ -1,3 +1,26 @@
-info = os.popen('ifconfig').read()
+def getIP()
+    info = os.popen('ifconfig').read()
 
-print(info)
+    print(info)
+
+
+def getPackageManager()
+    
+    ID_LIKE = "debian"
+
+    switch(N)
+    {
+        case 1: return "apt" if ID_LIKE == "debian"
+        break;
+        case 2: return "pacman -S" if ID_LIKE == "arch"
+        break;
+        case 3: return "pacman -S" if ID_LIKE == "red hat"
+        break;
+        default: return -1
+    }
+
+#------------------------------------------------------------
+
+
+getIP()
+manager = getPackageManager()
